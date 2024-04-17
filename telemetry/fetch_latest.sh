8136807b25835e53d334c91d7adb6c6a1d110e9c
Daniel <danirdd92@gmail.com>
2024-04-17 20:21:16 +0300

    initial commit

---
+++ telemetry/fetch_latest.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+USR=$1
+PWD=$2
+VERSION=$3
+
+
+if [[ "$VERSION" != "99-SNAPSHOT" ]]; then
+    curl -u ${USR}:${PWD} -O "http://artifactory:8082/artifactory/libs-release-local/com/lidar/analytics/maven-metadata.xml"
+    HIGHEST_PATCH=$(grep -oP "<version>${VERSION}\.[0-9]+</version>" maven-metadata.xml | grep -oP "${VERSION}\.[0-9]+" | sort -V | tail -n1)
+    VERSION=$HIGHEST_PATCH
+fi
+
+mvn dependency:get -Dartifact=com.lidar:analytics:$VERSION -Ddest=analytics.jar
